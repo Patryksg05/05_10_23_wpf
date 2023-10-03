0ec461aae7febedb577b66f772f16144a7eb519a
Patryk Maciak <maciakpatryk32@gmail.com>
2023-10-03 19:44:46 +0200

    left, right select from index image

--- Gallery/Gallery/MainWindow.xaml.cs
+++ Gallery/Gallery/MainWindow.xaml.cs
@@ -20,24 +20,51 @@ namespace Gallery
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> list = new List<string>();
+        int image_id = 0;
         public MainWindow()
         {
             InitializeComponent();
+            list.Add("images/forest1.jpg");
+            list.Add("images/forest2.jpg");
+            list.Add("images/forest3.jpg");
         }
 
         private void left_click(object sender, RoutedEventArgs e)
         {
-
+            //MessageBox.Show(list.Count.ToString());
+            image_id--;
+            if (image_id < 0)
+            {
+                image_id = list.Count - 1;
+            }
+            image.Source = new BitmapImage(new Uri(list[image_id], UriKind.Relative));
         }
 
         private void right_click(object sender, RoutedEventArgs e)
         {
-
+            image_id++;
+            if (image_id >= list.Count)
+            {
+                image_id = 0;
+            }
+            image.Source = new BitmapImage(new Uri(list[image_id], UriKind.Relative));
         }
 
         private void confirm_btn(object sender, RoutedEventArgs e)
         {
-
+            int num;
+            if (int.TryParse(image_id_text_box.Text, out num))
+            {
+                if (num >= 1 && num < list.Count+1)
+                {
+                    image.Source = new BitmapImage(new Uri(list[num-1], UriKind.Relative));
+                }
+                else
+                {
+                    MessageBox.Show("range!");
+                }
+            }
         }
     }
 }
