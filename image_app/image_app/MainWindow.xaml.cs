3e58008dda4d1a9d81118f624588c05a2b0f83af
Patryk Maciak <maciakpatryk32@gmail.com>
2023-10-04 16:05:06 +0200

    left side ready

--- image_app/image_app/MainWindow.xaml.cs
+++ image_app/image_app/MainWindow.xaml.cs
@@ -20,14 +20,46 @@ namespace image_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<String> list = new List<String>();
         public MainWindow()
         {
             InitializeComponent();
+            list.Add("images/forest1.jpg");
+            list.Add("images/forest2.jpg");
+            list.Add("images/forest3.jpg");
+            list.Add("images/forest4.jpg");
+            list.Add("images/forest5.jpg");
         }
 
-        private void confirm_btn(object sender, RoutedEventArgs e)
+        private void select_from_radio_click(object sender, RoutedEventArgs e)
         {
+            if(image1.IsChecked == true)
+            {
+                image.Source = new BitmapImage(new Uri("../../../images/forest1.jpg", UriKind.Relative));
+            }
+            else if(image2.IsChecked == true)
+            {
+                image.Source = new BitmapImage(new Uri("../../../images/forest2.jpg", UriKind.Relative));
+            }
+            else if(image3.IsChecked == true)
+            {
+                image.Source = new BitmapImage(new Uri("../../../images/forest3.jpg", UriKind.Relative));
+            }
+            else if(image4.IsChecked == true)
+            {
+                image.Source = new BitmapImage(new Uri("../../../images/forest4.jpg", UriKind.Relative));
+            }
+        }
 
+        private void select_from_index_btn(object sender, RoutedEventArgs e)
+        {
+            int index;
+            if (int.TryParse(index_text_box.Text, out index) && (index >= 1 && index < 6))
+            {
+                image.Source = new BitmapImage(new Uri(list[index-1], UriKind.Relative));
+            }
+            else
+                MessageBox.Show("range!");
         }
     }
 }
